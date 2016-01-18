0eb208ddd776d574d53e223a55a51d92bc630208
filips <filip.stamcar@hotmail.com>
2016-01-18 17:32:50 +0100

    Initial commit

---
+++ languages/slovenian.php
@@ -0,0 +1,5 @@
+<?php
+$GLOBALS['slovenian']["can't open"] = "Ne morem odpreti $file";
+$GLOBALS['slovenian']["ok"] = 'Zapisovanje uspešno';
+$GLOBALS['slovenian']["showClass/showOther"] = 'Ne najdem navtavitev showConfig on showOther';
+?>

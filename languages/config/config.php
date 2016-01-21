125a6bac4b9becd333d750fdc3bb6694943ad630
filips <filip.stamcar@hotmail.com>
2016-01-21 17:12:27 +0100

    Initial commit

---
+++ languages/config/config.php
@@ -0,0 +1,18 @@
+<?php
+class Config { 
+	public $display_offline_message = '1'; 
+	public $offline = '1'; //2
+	public $offline_message = 'hej'; //nej
+	public $offline_message2 = 'hej'; //nej
+	
+//addclass
+}
+
+
+$display_offline_message = "1"; 
+$offline = "1"; //2
+$offline_message = "hej"; //nej
+$offline_message2 = "hej"; //nej
+
+//addnoclass
+?>
---
+++ languages/config/languages/english.php
@@ -0,0 +1,5 @@
+<?php
+$GLOBALS['english']["can't open"] = 'Write successfully';
+$GLOBALS['english']["ok"] = "I can't open $file";
+$GLOBALS['english']["showClass/showOther"] = "I can't find the settings showClass and showOther";
+?>

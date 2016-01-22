a2c2724c963fa0ba3dcb1ed9beb01d55068570b5
fillips <filip.stamcar@hotmail.com>
2016-01-22 20:01:07 +0100

    brisanje zaradi napake

--- languages/config/languages/english.php
+++
@@ -1,5 +0,0 @@
-<?php
-$GLOBALS['english']["can't open"] = 'Write successfully';
-$GLOBALS['english']["ok"] = "I can't open $file";
-$GLOBALS['english']["showClass/showOther"] = "I can't find the settings showClass and showOther";
-?>

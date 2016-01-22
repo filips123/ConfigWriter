ca412838eb7b6e4d53535d35ecbc53604644711e
fillips <filip.stamcar@hotmail.com>
2016-01-22 20:01:14 +0100

    Delete slovenian.php

--- languages/config/languages/slovenian.php
+++
@@ -1,5 +0,0 @@
-<?php
-$GLOBALS['slovenian']["can't open"] = "Ne morem odpreti $file";
-$GLOBALS['slovenian']["ok"] = 'Zapisovanje uspešno';
-$GLOBALS['slovenian']["showClass/showOther"] = 'Ne najdem navtavitev showConfig on showOther';
-?>
